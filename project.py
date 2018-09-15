5fb65d6c41a72b1be5f5ede32056f41eec044ef7
Malay Shah <malay95@users.noreply.github.com>
2018-09-15 15:50:00 -0700

    Update project.py

--- project.py
+++ project.py
@@ -5,5 +5,4 @@ from app.models import User,Stats,UserActivity
 def make_shell_context():
 	return {'db':db, 'User':User, 'Stats':Stats, 'UserActivity':UserActivity}
 
-if __name__ == '__main__':
-	app.run()
+
